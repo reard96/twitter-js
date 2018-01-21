5dd626f50d0d25fc45ff3a0db2cc43567102b811
reard96 <reardon.danielj@gmail.com>
2018-01-21 16:20:17 -0500

    Listening on port 3000

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express(); // creates instance of an express application
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
